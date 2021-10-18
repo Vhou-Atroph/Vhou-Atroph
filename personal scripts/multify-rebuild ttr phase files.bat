358da94168153ec657ae56140320051478f46215
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2021-10-18 10:12:36 -0500

    ttr phase file batch files
    for extracting and rebuilding the phase files.

---
+++ personal scripts/multify-rebuild ttr phase files.bat
@@ -0,0 +1,18 @@
+@echo off
+multify.exe -cf phase_3.5.mf phase_3.5
+multify.exe -cf phase_3.mf phase_3
+multify.exe -cf phase_4.mf phase_4
+multify.exe -cf phase_5.5.mf phase_5.5
+multify.exe -cf phase_5.mf phase_5
+multify.exe -cf phase_6.mf phase_6
+multify.exe -cf phase_7.mf phase_7
+multify.exe -cf phase_8.mf phase_8
+multify.exe -cf phase_9.mf phase_9
+multify.exe -cf phase_10.mf phase_10
+multify.exe -cf phase_11.mf phase_11
+multify.exe -cf phase_13.mf phase_13
+multify.exe -cf winter_decorations.mf winter_decorations
+multify.exe -cf winter_music.mf winter_music
+multify.exe -cf winter_snow.mf winter_snow
+echo Done!
+pause
--- personal scripts/multify-ttr phase files.bat
+++ personal scripts/multify-ttr phase files.bat
@@ -11,7 +11,6 @@ multify.exe -xf phase_9.mf
 multify.exe -xf phase_10.mf
 multify.exe -xf phase_11.mf
 multify.exe -xf phase_13.mf
-multify.exe -xf phase_13.mf
 multify.exe -xf winter_decorations.mf
 multify.exe -xf winter_music.mf
 multify.exe -xf winter_snow.mf
