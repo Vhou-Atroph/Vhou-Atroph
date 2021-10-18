bed5734a62810e8502712b7b24c37e9c83101825
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2021-10-18 00:36:51 -0500

    New section
    for personal scripts

---
+++ personal scripts/multify-ttr phase files.bat
@@ -0,0 +1,19 @@
+@echo off
+multify.exe -xf phase_3.5.mf 
+multify.exe -xf phase_3.mf 
+multify.exe -xf phase_4.mf 
+multify.exe -xf phase_5.5.mf 
+multify.exe -xf phase_5.mf 
+multify.exe -xf phase_6.mf 
+multify.exe -xf phase_7.mf 
+multify.exe -xf phase_8.mf 
+multify.exe -xf phase_9.mf 
+multify.exe -xf phase_10.mf
+multify.exe -xf phase_11.mf
+multify.exe -xf phase_13.mf
+multify.exe -xf phase_13.mf
+multify.exe -xf winter_decorations.mf
+multify.exe -xf winter_music.mf
+multify.exe -xf winter_snow.mf
+echo Done!
+pause
